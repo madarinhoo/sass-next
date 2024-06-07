9f42e3a601708c18b7903ef230c8b996246bc9d9
Jbtzcodes <laminutedecode@gmail.com>
2024-06-07 13:35:01 +0200

    mise a jour buttons

--- app/components/ButtonsProviders.tsx
+++ app/components/ButtonsProviders.tsx
@@ -1,17 +1,17 @@
 
 import {signIn} from "next-auth/react"
 import { Button } from "@/components/ui/button"
-import { Github, Mail} from 'lucide-react';
+
 export default function ButtonsProviders() {
   return (
     <>
       <div className="flex flex-col gap-4">
-        <Button onClick={async () => await signIn('mail')} className="flex items-center gap-2">
-          <Mail/>
+        <Button onClick={async () => await signIn('gmail')} className="flex items-center gap-2">
+          
           <span>Continuer avec Google</span>
         </Button>
         <Button onClick={async() =>  await signIn('github')} className="flex items-center gap-2">
-        <Github />
+    
           <span>Continuer avec GitHub</span>
         </Button>
       </div>
