0499335ca9f182a93861fe55aba020e99ff0a169
Jbtzcodes <laminutedecode@gmail.com>
2024-06-07 13:29:29 +0200

    mise a jour

--- app/components/ButtonsProviders.tsx
+++ app/components/ButtonsProviders.tsx
@@ -6,11 +6,11 @@ export default function ButtonsProviders() {
   return (
     <>
       <div className="flex flex-col gap-4">
-        <Button onClick={() => signIn('mail')} className="flex items-center gap-2">
+        <Button onClick={async () => await signIn('mail')} className="flex items-center gap-2">
           <Mail/>
           <span>Continuer avec Google</span>
         </Button>
-        <Button onClick={() => signIn('github')} className="flex items-center gap-2">
+        <Button onClick={async() =>  await signIn('github')} className="flex items-center gap-2">
         <Github />
           <span>Continuer avec GitHub</span>
         </Button>
